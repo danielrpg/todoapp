614a65f5d86982847debdcf94dccd45a5b677143
danielrpg <daniel.fernandezgp@gmail.com>
2016-12-11 21:18:12 -0400

    Cambios para crear el servidor en el back end

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var path        = require('path');
 var morgan      = require('morgan');
 var mongoose    = require('mongoose');
 
+var mainRouter  = require('./routes/index');
+var apiRouter  = require('./routes/api'); 
+
 const PORT = 8000 || process.env.PORT;
 var DB = "mongodb://localhost:27017/tododb";
 
@@ -12,10 +15,20 @@ app.use(morga('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use('/', mainRouter);
+app.use('/api', apiRouter);
+
 mongoose.connect(DB, function (err){
     if(err){
         return err;
     }else{
         console.log('successfully connected to '+DB);
     }
+});
+
+app.set('views', __dirname+'/client/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/client'));
+app.listen(PORT, function(){
+    console.log('server running in port '+ PORT);
 });
